65d45d6325e732e9b108f497e93e11f733eb8925
Thomas Le <le.thomas.vu@gmail.com>
2018-01-11 11:00:44 -0800

    added env

--- config/database.js
+++ config/database.js
@@ -1,8 +1,7 @@
-const crypto = require('crypto').randomBytes(256).toString('hex'); // Provides cryptographic functionality (OpenSSL's hash, HMAC, cipher, decipher, sign and verify functions)
+const crypto = require('crypto').randomBytes(256).toString('hex');
 
-// Export config object
 module.exports = {
-    uri: 'mongodb://deadpools:<idontknow>@ds249787.mlab.com:49787/password-checker', // Databse URI and database name
-    secret: crypto, // Cryto-created secret
-    db: 'MEAN-project' // Database name
+    uri: process.env.databaseUri,
+    secret: crypto,
+    db: process.env.databaseName
 }
